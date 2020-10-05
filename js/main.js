dc36ea4b0894be15396a81f53ce55a3eb5509f47
AkbarjonTojiyev <tojiyev.akbarjon@gmail.com>
2020-10-05 19:16:32 +0500

    Fix header for mobile screen and make deliver section responsive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 var elButton = document.querySelector(".site-header__burger");
+var elInner = document.querySelector(".nav-inner");
 var elNav = document.querySelector(".nav");
 
 elButton.addEventListener("click", function() {
-	elNav.classList.toggle("nav--toggle");
+	elNav.classList.toggle("nav--dark");
+	elInner.classList.toggle("nav-inner--toggle");
 	elButton.classList.toggle("site-header__burger--close");
 });
