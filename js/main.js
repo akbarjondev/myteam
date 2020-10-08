1e6caf0da43b6a9ff3bb52481f412f62f80e4f4c
AkbarjonTojiyev <tojiyev.akbarjon@gmail.com>
2020-10-08 15:59:41 +0500

    Add directors section and responsivnes WIP

--- js/main.js
+++ js/main.js
@@ -7,3 +7,15 @@ elButton.addEventListener("click", function() {
 	elInner.classList.toggle("nav-inner--toggle");
 	elButton.classList.toggle("site-header__burger--close");
 });
+
+var elPlusButton = document.querySelectorAll(".directors__item-button");
+var elDirectorsItem = document.querySelector(".directors__item");
+
+
+elPlusButton.forEach(elToggle);
+
+function elToggle(e) {
+	e.addEventListener("click", function() {
+		e.classList.toggle("shape");
+	});
+}
